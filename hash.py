8bfb793308ee050e7148246cd55fe2267408db3b
shivakumarzishnuviknesh7 <shivakumar.zishnuviknesh@gmail.com>
2024-09-28 20:32:34 +0200

    hash updated

---
+++ hash.py
@@ -0,0 +1,13 @@
+data ={}
+
+data["data1"]= 25124
+data["data2"]= 241
+data["data3"]= 277
+data["data4"]= 259
+data["data5"]= 244
+data["data6"]= 25
+
+if "data1" in data:
+    print("key found")
+else:
+    print("Key not found")
